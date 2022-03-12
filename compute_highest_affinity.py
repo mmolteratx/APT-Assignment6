4997e05dae3a52d63fa2924246c3f904a74eda82
Matt Molter <mmolteratx@gmail.com>
2022-03-12 14:48:42 -0600

    Modify for code coverage decrease

--- compute_highest_affinity.py
+++ compute_highest_affinity.py
@@ -30,3 +30,56 @@ def highest_affinity(site_list, user_list, time_list):
                 affinities[pair] += 1
 
     return max(affinities, key=affinities.get)
+
+def useless():
+    num_lines = 10000
+    num_users = 1000
+
+    site_list = randomized_input.randomized_site_list(num_lines)
+    user_list = randomized_input.randomized_user_list(num_lines, num_users)
+    time_list = range(0, num_lines)
+
+    computed_result = compute_highest_affinity.highest_affinity(
+        site_list, user_list, time_list)
+    expected_result = ("facebook", "google")
+
+    assert computed_result == expected_result
+    print("Successfully passed {}!".format(os.path.basename(__file__).split(".")[0]))
+
+def useless2():
+    a = "a"
+    b = "b"
+    c = "c"
+    d = "d"
+    e = "e"
+    f = "f"
+    g = "g"
+    h = "h"
+
+    A = "A"
+    B = "B"
+    C = "C"
+    D = "D"
+    E = "E"
+    F = "F"
+    G = "G"
+    H = "H"
+
+    user_list = [
+        A, C, H, A, C, D, E, F, G, C, D, E, F, G, H, C, B, D, E, F, G, C, G, A, D,
+        E, H, A, C, E, H
+    ]
+    site_list = [
+        a, a, a, b, b, b, b, b, b, c, c, c, c, c, c, d, e, e, e, e, e, f, f, g, g,
+        g, g, h, h, h, h
+    ]
+
+    time_list = range(0, 31)
+
+    computed_result = compute_highest_affinity.highest_affinity(
+        site_list, user_list, time_list)
+    expected_result = (b, c)
+
+    assert computed_result == expected_result
+    print("Successfully passed {}!".format(os.path.basename(__file__).split(".")[0]))
+
